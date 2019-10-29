9628cfadf28c672519f83ae3743e7f18bde619f2
unknown <marta.razvan@gmail.com>
2019-10-29 21:01:08 +0200

    Updates

---
+++ precache-manifest.6640e50036085472c6b960c6c4b53d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43a22b7845e52e79756c9d74e360695",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "0b00f7920c85b916fc19",
+    "url": "/Robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a3a6596904fecf061fb6",
+    "url": "/Robots/static/css/main.c94fd851.chunk.css"
+  },
+  {
+    "revision": "0b00f7920c85b916fc19",
+    "url": "/Robots/static/js/2.f8c4f52c.chunk.js"
+  },
+  {
+    "revision": "a3a6596904fecf061fb6",
+    "url": "/Robots/static/js/main.b4f5104b.chunk.js"
+  },
+  {
+    "revision": "20c003b391574b5005b0",
+    "url": "/Robots/static/js/runtime-main.4f88075a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
